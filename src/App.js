1d8ab4b6ba7e6885cb3ffeeeba1576291d76c442
vandit-bera <vanditbera@gmail.com>
2022-11-05 09:07:40 +0530

    Add Logic

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
     <div className='container'>
       <h1>Basic counter App</h1>
       <h3>Count is: {count}</h3>
-      <button onClick={() => count > -10 ? setCount(count - 1) : ""}>Decrease</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      {/* <button onClick={() => count > -10 ? setCount(count - 1) : ""}>Decrease</button> */}
       <button onClick={() => setCount(0)}>Reset</button>
-      <button onClick={() => count >= 10 ? "" : setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      {/* <button onClick={() => count >= 10 ? "" : setCount(count + 1)}>Increase</button> */}
       <p className='author'>- Vandit Bera</p>
     </div>
   );
